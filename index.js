044ae77541f2990edb20a1c4ebc779f385171196
AbroShahzeb <khizr77@gmail.com>
2025-01-23 18:27:31 +0500

    Database connected

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 const app = express();
 
 import { configDotenv } from "dotenv";
+import { connectDB } from "./utils/db.js";
 configDotenv();
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello world" });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on PORT`, process.env.PORT || 3000);
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on PORT`, process.env.PORT || 3000);
+  });
 });
