d04f57bc8fdadd9b0d4513eb5d80291fc8df1feb
AbroShahzeb <khizr77@gmail.com>
2025-01-25 19:03:43 +0500

    Implemented Error Handling

---
+++ controllers/errorController.js
@@ -0,0 +1,59 @@
+import AppError from "../utils/appError.js";
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateValueDB = (err) => {
+  const val = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value ${val}. Please try another one.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors?.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log("Error", err);
+    return res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
+export default (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if ((error.name = "CastError")) error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateValueDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
+};
--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 import express from "express";
 const app = express();
 
+process.on("uncaughtException", (err) => {
+  console.log("UNHANDLED EXCEPTION 🚚! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 import { configDotenv } from "dotenv";
 import { connectDB } from "./utils/db.js";
+import AppError from "./utils/appError.js";
+import globalErrorHandler from "./controllers/errorController.js";
 configDotenv();
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Hello world" });
 });
 
-connectDB().then(() => {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on PORT`, process.env.PORT || 3000);
+connectDB();
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on PORT`, process.env.PORT || 3000);
+});
+
+app.all("*", (req, res, next) => {
+  next(
+    new AppError(`Cannot find ${req.originalUrl} on this server! testtt`, 404)
+  );
+});
+
+app.get("/", (req, res, next) => {
+  res.json("Some middleware in the between");
+});
+
+app.use(globalErrorHandler);
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION 🔥! Shutting down...");
+  server.close(() => {
+    process.exit(1);
   });
 });
